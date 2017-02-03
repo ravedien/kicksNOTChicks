cd40c3b167c73ca1c5fa7badd34e1fb00126cbf8
ravedien <hilbert@azneita.org>
2017-02-03 11:20:26 +0800

    add working copy for monitoring

--- monitoring/src/main/java/monitoring/MonitoringApplication.java
+++ monitoring/src/main/java/monitoring/MonitoringApplication.java
@@ -2,12 +2,20 @@ package monitoring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
 
-@SpringBootApplication
-@EnableTurbineStream
+
+/*Hystrix Dashboard usage
+Usage will be on edge service
+pattern: host:port/hystrix.stream
+ex: http://localhost:8080/hystrix.stream
+*/
+
+@EnableDiscoveryClient
 @EnableHystrixDashboard
+@SpringBootApplication
 public class MonitoringApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MonitoringApplication.class, args);
